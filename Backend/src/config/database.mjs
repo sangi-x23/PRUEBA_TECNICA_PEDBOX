611c389cef8612799fae23780bb745735ee4ecde
sangi-x23 <ssg5670@gmail.com>
2025-09-08 21:22:29 -0500

    configuration for vercel

--- Backend/src/config/database.mjs
+++ Backend/src/config/database.mjs
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || "pedbox",
-    process.env.DB_USER || "root",
-    process.env.DB_PASS || "2020",
+    process.env.DB_NAME || "bhkogitpbj5of2icgujp",
+    process.env.DB_USER || "ux8ljgynhhz67oae",
+    process.env.DB_PASS || "28ycPAsQMTeF1UfMHP2W",
     {
-        host: process.env.DB_HOST || "localhost",
+        host: process.env.DB_HOST || "bhkogitpbj5of2icgujp-mysql.services.clever-cloud.com",
         port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
         logging: false
--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -21,16 +21,20 @@ app.get("/", (req, res) => {
   res.send("API Funcionando!");
 });
 
-const start = async () => {
+if (process.env.NODE_ENV !== "production") {
+  const start = async () => {
     try {
-        await sequelize.authenticate();
-        console.log("Conexión a la base de datos establecida");
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+      await sequelize.authenticate();
+      console.log("Conexión a la base de datos establecida");
+      const PORT = process.env.PORT || 3000;
+      app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
     } catch (error) {
-        console.error("No se pudo conectar a la base de datos:", error);
-        process.exit(1);
+      console.error("No se pudo conectar a la base de datos:", error);
+      process.exit(1);
     }
-};
+  };
 
-start();
+  start();
+}
+
+export default app;
