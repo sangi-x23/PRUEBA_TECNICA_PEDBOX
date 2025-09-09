13a035100ba9768ebfa9deb86e1d9fc01e4691fb
sangi-x23 <ssg5670@gmail.com>
2025-09-08 21:42:01 -0500

    Bugs fixed

--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -20,21 +20,18 @@ app.use("/api", subredditRouter);
 app.get("/", (req, res) => {
   res.send("API Funcionando!");
 });
-/*
-if (process.env.NODE_ENV !== "production") {
-  const start = async () => {
+
+const start = async () => {
     try {
-      await sequelize.authenticate();
-      console.log("Conexión a la base de datos establecida");
-      const PORT = process.env.PORT || 3000;
-      app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+        await sequelize.authenticate();
+        console.log("Conexión a la base de datos establecida");
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
     } catch (error) {
-      console.error("No se pudo conectar a la base de datos:", error);
-      process.exit(1);
+        console.error("No se pudo conectar a la base de datos:", error);
+        process.exit(1);
     }
   };
 
-  start();
-}
-*/
-export default app;
+start();
+
